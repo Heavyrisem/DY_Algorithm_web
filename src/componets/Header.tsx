3754aebf3e6af18f4d54cefd85965f78b7b5c1c0
heavyrisem <heavyrisem@gmail.com>
2021-09-11 00:58:58 +0900

    Header, Sidemenu

--- src/componets/Header.tsx
+++ src/componets/Header.tsx
@@ -6,19 +6,26 @@ import Sidemenu from "./Sidemenu";
 
 function Header() {
     const Paths = ["ChellengeName"];
+    const RightAligen: React.CSSProperties = {
+        margin: 'auto',
+        marginLeft: 'auto',
+        marginRight: 0,
+        display: 'flex'
+    }
 
     return (
         <header>
             <img src={Logo} className="Logo" />
-            <span className="Title">Dyalgorithm</span>
+            <span className="Title">DYalgorithm</span>
             {Paths.map((Path, idx) => (
                 <>
-                    {(idx == Paths.length-1)&& <span className="PathDevider">/</span>}
-                    <span className="Path">{Path}</span>
+                    {(idx == Paths.length-1)&& <span key={idx} className="PathDevider">/</span>}
+                    <span key={Path} className="Path">{Path}</span>
                 </>
-            ))};
-
-            <Sidemenu />
+            ))}
+            <div style={RightAligen}>
+                <Sidemenu />
+            </div>
         </header>
     )
 }
--- src/componets/Sidemenu.tsx
+++ src/componets/Sidemenu.tsx
@@ -1,15 +1,104 @@
-import React, { useState } from "react";
+import React, { EventHandler, useEffect, useRef, useState } from "react";
 import Button from "../style/img/MenuButton.svg";
+import '../style/Sidemenu.css';
 
 function Sidemenu() {
-    const [showMenu, setShowMenu] = useState<boolean>(false);
+    const [showMenu, setShowMenu] = useState<boolean>(true);
+
+    function asdf(e: React.ChangeEvent<any>) {
+        console.log(e.target.value);
+    }
 
     return (
         <>
-            {!showMenu&&
-                <img src={Button} />
-            }
+            
+            <img src={Button} className="SidemenuButton" onClick={()=>setShowMenu(!showMenu)} style={{marginRight: '1rem'}} />
+
+            {showMenu&& <div className="DarkBackground" />}
+            <div className={"Sidemenu" + (showMenu? " Open":"")}>
+                <div className="Header">
+                    <span className="UserNickname">사용자 닉네임</span>                                
+                    <img src={Button} className="SidemenuButton" onClick={()=>setShowMenu(!showMenu)} />
+                </div>
+
+                <Section name="Section">
+                    <Text name="설정_텍스트" onValue={asdf} />
+                    <Dropdown name="설정_드롭다운_메뉴" onValue={asdf}>
+                        <option value="123">123</option>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                    </Dropdown>
+                    <Toggle name="설정_토글" onValue={(e)=>{console.log(e.target.checked)}} />
+                </Section>
+            </div>
+        
         </>
+    )
+}
+
+interface Section_P {
+    name: string
+    children: JSX.Element[] | JSX.Element
+}
+function Section(props: Section_P) {
+    let childrens = props.children;
+    if (!(childrens instanceof Array)) childrens = [childrens];
+    
+    return (
+        <div className="Section" >
+            <div className="Name">{props.name}</div>
+            <div className="Options">
+                {childrens.map((child) => child)}
+            </div>
+        </div>
+    )
+}
+
+interface Text_Option_P {
+    name: string
+    onValue: React.ChangeEventHandler<HTMLInputElement>
+}
+function Text(props: Text_Option_P) {
+    return (
+        <div className="Option">
+            <span className="name">{props.name}</span>
+            <div className="input"><input type="text" onChange={props.onValue} /></div>
+        </div>
+    )
+}
+
+interface Dropdown_Option_P {
+    name: string
+    onValue: React.ChangeEventHandler<HTMLSelectElement>
+    children: JSX.Element[] | JSX.Element
+}
+function Dropdown(props: Dropdown_Option_P) {
+    let childrens = props.children;
+    if (!(childrens instanceof Array)) childrens = [childrens];
+
+    return (
+        <div className="Option">
+            <span className="name">{props.name}</span>
+            <select onChange={props.onValue}>
+                {childrens.map((child) => child)}
+            </select>
+        </div>
+    )
+}
+
+interface Toggle_Option_P {
+    name: string
+    onValue: React.ChangeEventHandler<HTMLInputElement>
+}
+function Toggle(props: Toggle_Option_P) {
+    return (
+        <div className="Option">
+            <span className="name">{props.name}</span>
+            <label className="switch">
+                <input type="checkbox" onChange={props.onValue} />
+                <span className="slider round"></span>
+            </label>
+        </div>
     )
 }
 
